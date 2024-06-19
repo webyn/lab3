930efdd82fe8a9f78ec055128f8dbec3eaa15673
Robyn Fruto <rnfruto@student.apc.edu.ph>
2024-06-19 20:04:38 +0800

    ci4 static pages tutorial

---
+++ ci4/app/Controllers/Pages.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controllers;
+
+// Add this line to import the class.
+use CodeIgniter\Exceptions\PageNotFoundException;
+
+class Pages extends BaseController
+{
+    public function index()
+    {
+        return view('welcome_message');
+    }
+
+   public function view(string $page = 'home')
+    {
+        if (! is_file(APPPATH . 'Views/pages/' . $page . '.php')) {
+            // Whoops, we don't have a page for that!
+            throw new PageNotFoundException($page);
+        }
+
+        $data['title'] = ucfirst($page); // Capitalize the first letter
+
+        return view('templates/header', $data)
+            . view('pages/' . $page)
+            . view('templates/footer');
+    }
+}
